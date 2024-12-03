3e4e2df2e8253dfe359e5ee243b5290d6faefbb3
Bruno Gonçalves Rodrigues <59949430+BrunoR1907@users.noreply.github.com>
2024-12-02 22:49:02 -0300

    Update tvbox.py

--- tvbox.py
+++ tvbox.py
@@ -2,7 +2,7 @@ print("Bem-vindo à calculadora de soma!")
 
 a = int(input("digite o primeiro número: "))
 b = int(input("digite o segundo número: "))
+# Cole o código na linha abaixo:
 
-#
 
 print("O resultado da soma é:", res)
