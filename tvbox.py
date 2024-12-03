ade523d8124ceef08ef38138a8ac56f15342906e
Bruno Gonçalves Rodrigues <59949430+BrunoR1907@users.noreply.github.com>
2024-12-02 21:43:02 -0300

    Add files via upload

--- tvbox.py
+++ tvbox.py
@@ -1,1 +1,8 @@
-print("Hello World!")
+print("Bem-vindo à calculadora de soma!")
+
+a = int(input("digite o primeiro número: "))
+b = int(input("digite o segundo número: "))
+
+#
+
+print("O resultado da soma é:", res)
